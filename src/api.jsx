3b1967acb1fb893fbb652b6cc67508e213f215d6
Milan Saxena <46219948+MilanS56@users.noreply.github.com>
2026-02-05 14:02:18 +0530

    Update api.jsx

--- src/api.jsx
+++ src/api.jsx
@@ -2,7 +2,7 @@ export async function sendAudio(word, blob) {
   const form = new FormData();
   form.append("audio", blob);
 
-  const res = await fetch(`https://pronunciation-engine-backend.onrender.com/api/pronounce/${word}
+  const res = await fetch(`https://pronunciation-engine-backend-1.onrender.com/api/pronounce/${word}
 `, {
     method: "POST",
     body: form
